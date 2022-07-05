1859eba5bfe3767bbfa879e9df6615ddf018dde2
marmy1634 <108736679+marmy1634@users.noreply.github.com>
2022-07-05 09:36:43 -0400

    Add files via upload

---
+++ lists.py
@@ -0,0 +1,84 @@
+# 1
+# Try this. Do you expect it to print “water” or “chair”?
+thelist = ["water", "chair", "mug", "mouse"]
+print(thelist[1])
+
+# 2
+# Modify the previous example to print the fourth element of the list (“mouse”).
+thelist = ["water", "chair", "mug", "mouse", "bug", "amogus"]
+print(thelist[3])
+
+
+# 3
+# Try this. Do you expect an error?
+print(thelist[-1])
+
+# 4
+# Add a few elements to thelist. Then do this again:
+print(thelist[-1])
+
+
+# 5
+# Now make it print the second-to-last element of thelist.
+print(thelist[2])
+
+# 6
+# Try this. It’s a little counterintuitive.
+print(thelist[0:3])
+
+
+# 7
+# Try this.
+print(thelist[1:3])
+
+
+# 8
+# Modify the previous example to print elements 0,1,2,3. (Hint: it’s not going to be [0:3], but it’s close to that.)
+print(thelist[0:4])
+
+# 9
+# Try this.
+print(thelist[2:])
+
+# 10
+# Modify the previous example to print all but the first two items.
+# Pretend you don’t know how long the list is. (You shouldn’t need to.)
+
+
+# 11
+# Try this.
+letters = ["a", "b", "c", "d", "e"]
+# letters[0] = "ROCKET"
+# letters[1] = "ROCKET"
+letters[-2] = "ROCKET"
+
+# 12
+# Modify the previous example to replace “b” with “ROCKET” instead of replacing “a”.
+
+
+# 13
+# Modify the previous example to replace the second-to-last element with “ROCKET”.
+
+
+# 14
+# Try this.
+name = "Becky"
+print(name[2])
+
+# 15
+# Modify the previous example to print the “c” of “Becky”.
+
+
+# 16
+# Try this.
+names = ["Sarah", "Tom", "Harry", "Susan", "Becky"]
+# print(names[4][3])
+# print(names[0][-1])
+print(names[-1][-1])
+
+
+# 17
+# Modify the previous example to print the last letter of Sarah’s name (using string indexing).
+
+# 18
+# Modify the previous example to print the last letter of the last name in the list. Use negative indexing.
